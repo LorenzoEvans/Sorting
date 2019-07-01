8394b3343bab3b4e166f6bbef3969632e456d283
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-07-01 19:36:29 -0400

    Working on insertion sort.

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -3,41 +3,52 @@
 def insertion_sort(arr):
     for i in range(1, len(arr)):
         cur_val = arr[i]
-
-
-
-def selection_sort( arr ):
-    # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = 0
-        # first element of arr
-
-        smallest_index = cur_index
-
-        while smallest_index > cur_index:
-
-
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
-
-        # TO-DO: swap
-
-
-
-
+        index = i
+        print("Outer index:", index)
+        while index > 0 and arr[index - 1] > cur_val:
+            arr[index] = arr[index - 1]
+            index -= 1
+            print("Inner index:", index)
+        arr[index] = cur_val
     return arr
 
 
-# TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
+rand_arr = [1, 4, 6, 12, 5, 2]
 
-    return arr
+print(insertion_sort(rand_arr))
 
-
-# STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
-
-    return arr
+# def selection_sort( arr ):
+#     # loop through n-1 elements
+#     for i in range(0, len(arr) - 1):
+#         cur_index = 0
+#         # first element of arr
+#
+#         smallest_index = cur_index
+#
+#         while smallest_index > cur_index:
+#
+#
+#         # TO-DO: find next smallest element
+#         # (hint, can do in 3 loc)
+#
+#
+#
+#
+#         # TO-DO: swap
+#
+#
+#
+#
+#     return arr
+#
+#
+# # TO-DO:  implement the Bubble Sort function below
+# def bubble_sort( arr ):
+#
+#     return arr
+#
+#
+# # STRETCH: implement the Count Sort function below
+# def count_sort( arr, maximum=-1 ):
+#
+#     return arr
