144a60289c864242e8d5039c4c5e38ecc0ed8989
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-03-10 16:43:03 -0400

    Taking some notes

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,16 +1,28 @@
 # TO-DO: Complete the selection_sort() function below 
 
 def insertion_sort(arr):
+
     for i in range(0, len(arr)):
+      # We generate a list of integers for every element in the array
         cur_val = arr[i]
-        print(cur_val)
+      # We select the first value in the array
         index = i
-        print("Outer index:", index)
+      # We track out index placement with a variable i
+
         while index > 0 and arr[index - 1] > cur_val:
+        # While our index is greater than 0, and the element in the array, located before our current index
+        # is greater than our current value, we want to do some operations.
             arr[index] = arr[index - 1]
+              # We want to replace whatever element is at our current index, with the one before it
             index -= 1
-            print("Inner index:", index)
+              # Then, we then subtract one from the index (why?)
+                # Because we want to re-start the sort through the rest on the list, starting with the changed element,
+                # in order to make sure we touch every item as we iterate.
+
         arr[index] = cur_val
+        # If index is not greater than zero, OR the element located at our current array index is not greater than our current value,
+        # We put the current value, at the index.
+
     return arr
 
 
